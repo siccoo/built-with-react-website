4d2b6843d13fba38ff60a2a4d735d94c4bca789d
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-01 15:05:44 +0100

    Changes made to the React Web app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import News from './components/News';
+import Navbar from './components/CustomNavBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+        <Navbar />
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/news" component={News} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,30 @@ export default class Home extends Component{
             <Grid>
                 <Jumbotron>
                     <h2>Welcome to Judic-O Couture.io</h2>
-                    <p>Understang the basics of react, react-bootstrap & react-router-dom.</p>
-                </Jumbotron>
-                <Link>
+                    <p>Using react, react-bootstrap & react-router-dom to build a Fashion Home.</p>
+                <Link to="/about">
                     <Button bsStyle="primary">About</Button>
                 </Link>
+                </Jumbotron>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                    <Image src="asset/belisco.jpg" circle className="profile-pic" />
+                    <h3>Mrs Gift Onyinye</h3>
+                    <p>In her words Judic-O Couture brands need to integrate social commitments into their strategies and focus on attracting community members and building a winning business require the focus to resist doing too many things at once, while finding the right blend of fashion and technology. That is we focus only on female wears.</p>
+                    </Col>
+
+                    <Col xs={12} sm={4} className="person-wrapper">
+                    <Image src="asset/ugonma.jpg" circle className="profile-pic" />
+                    <h3>Jessy Chigo</h3>
+                    <p>In her words Judic-O Couture brands need to integrate social commitments into their strategies and focus on attracting community members and building a winning business require the focus to resist doing too many things at once, while finding the right blend of fashion and technology. That is we focus only on female wears.</p>
+                    </Col>
+
+                    <Col xs={12} sm={4} className="person-wrapper">
+                    <Image src="asset/emmanuel.jpg" circle className="profile-pic" />
+                    <h3>Emmanuel Ike</h3>
+                    <p>In her words Judic-O Couture brands need to integrate social commitments into their strategies and focus on attracting community members and building a winning business require the focus to resist doing too many things at once, while finding the right blend of fashion and technology. That is we focus only on female wears.</p>
+                    </Col>
+                </Row>
             </Grid>
         );
     };
